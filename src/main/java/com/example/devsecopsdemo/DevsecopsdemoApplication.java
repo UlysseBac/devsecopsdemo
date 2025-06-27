90df0b611aff549fd95bf62b0f080f9719017018
UlysseBac <ulysse.bac@efrei.net>
2025-06-27 14:10:42 +0000

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/devsecopsdemo/DevsecopsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devsecopsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devsecopsdemo/DevsecopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsecopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
